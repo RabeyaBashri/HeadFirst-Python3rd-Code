15c57aea7d98fb0b4659491dfab4168707fd1a1e
Rabeya Bashri <rbashrirabu@gmail.com>
2025-05-03 10:00:44 +0600

    build webapp, working with flask, jinja2

---
+++ ch_6/app.py
@@ -0,0 +1,90 @@
+"""To create a web app, proceed with the followings
+1. Create webapp folder
+2. Create app.py, templates folder, static folder(not utilized) inside webapp folder
+3. copy swimclub.py, hfpy_utils.py
+4. install flask () : %pip install flask --upgrade #***[for development server only, NOT for production]***
+5. install jinja2 extension : pip install Jinja2 
+6. Create base.html, index.html, selectswimmer.html, selectswimmersevent.html inside templares folder
+7. Modify swimclub - produceBarChart method 
+8. VS Code menu : Run > Start Debugging > Hit in Browser : http://127.0.0.1:5000 
+"""
+
+#Import all the required modules
+from flask import Flask, render_template, session, request
+import os
+import swimclub
+
+#Initialize Flask object to create the very webapp 
+app=Flask(__name__)
+app.secret_key = "Try something hard..."
+
+#Create session["swimmers"] to access swimmers name and filename in a dictionary format from any method
+def populateSwimmersData():
+    #create only for not exists case
+    if "swimmers" not in session:
+       
+        #get files
+        swimFiles = os.listdir(swimclub.FOLDER)
+        swimFiles.remove(".DS_Store")
+
+        #initialize session["swimmers"] dictionary
+        session["swimmers"] = {}
+
+        #populate swimmers data (key - swimmer name, value  - file name)
+        for file in swimFiles:
+            SwimmerName = file.split("-")[0]
+            if SwimmerName not in session["swimmers"] :
+                session["swimmers"][SwimmerName] = []
+            session["swimmers"][SwimmerName].append(file)  
+        
+
+
+#app decorator @, determines which URL(s) is going to call which server method defined just below it 
+
+#index.html
+@app.get("/")
+def index():
+    return render_template("index.html",
+                           title= "Swimclub System")
+
+#URL - /swiimers >>> selectswimmer.html - to display  swimmer name list 
+@app.get("/swimmers")
+def displaySwimmers():
+    populateSwimmersData()
+    return render_template("selectswimmer.html",
+                           title="Select a swimmer",
+                           url="/showfiles",
+                           select_name="Swimmer",
+                           select_id="Swimmer",
+                           data=sorted(session["swimmers"]))
+
+#URL - /showfiles >>> selectswimmersevent.html - to display  selected swimmer's file list
+@app.post("/showfiles")
+def displaySwimmerFiles():
+    populateSwimmersData()
+    swimmerName=request.form["Swimmer"]
+    filelist = session["swimmers"][swimmerName]
+    dictFileEvent = {}
+    for file in filelist:
+            swimmer, ageGroup, distance, stroke,*_ = swimclub.readSwimData(file)
+            event = f"{swimmer} (Under {ageGroup} {distance} {stroke})"
+            dictFileEvent[file] = event
+            
+    return render_template("selectswimmersevent.html",
+                        title="Select an  event",
+                        url="/showbarchart",
+                        select_name="file",
+                        select_id="file",
+                        data=dictFileEvent)
+
+#URL - /showbarchart >>> templates/ to display selected swimmer's file's bar chart 
+@app.post("/showbarchart")
+def showBarChart():
+    populateSwimmersData()
+    fileName=request.form["file"]
+    location = swimclub.produceBarChart(fileName,"templates/")
+    return  render_template(location.split("/")[-1])
+
+#run the very webapp
+if __name__ == "_main_":
+    app.run()
---
+++ ch_6/swimclub.py
@@ -0,0 +1,121 @@
+import statistics
+import hfpy_utils
+
+#FOLDER, CHARTS variable is containing constant value, it's no rule but convention
+FOLDER =  "swimdata/"
+CHARTS = "charts/"
+
+def readSwimData(fileName) :
+
+        #remove .txt by removesuffix, return list by splitting string value
+        #unpacking  or multiple assignmenet to the swimmer's info variables "swimmer, ageGroup, distance, stroke"
+        swimmer, ageGroup, distance, stroke = fileName.removesuffix(".txt").split("-")
+
+        #get swimmers time records in list "times"
+        with open(FOLDER + fileName) as file :
+            lines = file.readlines()
+            #intentionally takes only line 1, remove \n by strip() and split times using comma to return list
+            times = lines[0].strip().split(",")
+
+        #process time records to calculate average
+
+        #step - 1 converts all the time records into hundredths and stores in a list
+        convertTimeRecords = []
+
+        for time in times :
+            
+            if ":" in time :
+                minute, rest = time.split(":")
+                seconds, hundredths = rest.split(".")
+            else : 
+                 minute = 0
+                 seconds, hundredths = time.split(".")
+
+            convertedTime = (int(minute) * 60 * 100 ) + (int(seconds) * 100) + int(hundredths)
+            convertTimeRecords.append(convertedTime)
+
+        #step -2 calculate average
+
+        #get numerical hundredth avarage
+        average = statistics.mean(convertTimeRecords)
+
+        #but we need to show average in time format, so convert numerical to time format average
+        #convertedAverageTime = round(average/100,2)
+        #strConvertedAverageTime = str(convertedAverageTime)
+        #Using f-string format specifier
+        minute_second, hundredths = f"{(average/100):.2f}".split(".")
+        minute_second =  int(minute_second)
+        minutes = minute_second // 60 #floor division //
+        seconds = minute_second - minutes * 60
+
+        #convertedAverageTime = str(minutes) + ":" + str(seconds) + "." + str(hundredths)
+        #Using f-string instead of + to concatenate; 1:2:20 >> 1:02.20 (:0>2)
+        convertedAverageTime = f"{minutes} : {seconds:0>2} . {hundredths}"
+
+        return swimmer, ageGroup, distance, stroke, times, convertedAverageTime,convertTimeRecords
+
+#By invoking produceBarChart function, Coach can generate create any bar chart for the corresponding swimmer from filename
+def produceBarChart(fileName, location=CHARTS) :
+     
+    swimmer, ageGroup, distance, stroke,times,convertedAverageTime,convertTimeRecords = readSwimData(fileName)
+
+     #To calculate bar width for SVG, set values
+    barFromMin, barFromMax, barToMin, barToMax = [0,max(convertTimeRecords),0,350]
+
+     #Ordering bar 
+    times.reverse()
+    convertTimeRecords.reverse()
+
+     #Create HTML page data
+
+    title = f"{swimmer} (Under {ageGroup} {distance} {stroke})"
+
+    header = f"""
+                <!DOCTYPE html>
+                    <html>
+                        <head>
+                            <title>{title}</title>
+                        </head>
+             """
+    svg = ""
+     
+    for n, t in enumerate(times):
+        barWidth = hfpy_utils.convert2range(convertTimeRecords[n],barFromMin, barFromMax, barToMin, barToMax)
+        svg = svg + f"""
+                        <svg height="30" width="400">
+                                <rect height="30" width="{barWidth}" style="fill:rgb(0,0,,255)">
+                        </svg>
+                        {t}
+                        <br/>        
+                    """
+            
+    body = f"""
+                <body>
+                    <h3>{title}</h3>
+                    {svg}
+                
+            """
+    footer = f"""
+                        <p>Average time : {convertedAverageTime}</p>
+                </body>
+             </html>
+             """
+    page = header + body + footer
+
+    #Create HTML FILE
+    filePath = f"{location}{fileName.removesuffix('.txt')}.html"
+    with open(filePath,"w") as fl:
+        print(page,file=fl)
+
+    return filePath
+
+
+
+
+
+
+
+                    
+
+
+
